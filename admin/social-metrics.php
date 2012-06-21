cc2b27c35af1d9f37557a3f9e2c731df342b3565
Jon Bishop <jonbish@gmail.com>
2012-06-20 21:04:20 -0400

    Integrated sharrre for metrics

--- admin/social-metrics.php
+++ admin/social-metrics.php
@@ -4,7 +4,8 @@ class SocializeMetrics {
 
     function SocializeMetrics() {
         if (is_admin()) {
-            add_filter('socialize_settings_tabs_array', array(&$this, 'social_tab'));
+            add_filter('socialize_settings_tabs_array', array(&$this, 'social_tab'));\
+            add_action('admin_print_scripts', array(&$this, 'admin_scripts'));
         }
     }
 
@@ -16,6 +17,11 @@ class SocializeMetrics {
         return $tabs;
     }
     
+    function admin_scripts(){
+        wp_enqueue_script('sharrre', SOCIALIZE_URL . 'libs/sharrre/jquery.sharrre-1.3.2.min.js');
+        wp_enqueue_script('sharrre-custom', SOCIALIZE_URL . 'libs/sharrre.js');
+    }
+    
     //=============================================
     // Metrics page options
     //=============================================
@@ -23,43 +29,47 @@ class SocializeMetrics {
         $socialize_settings = self::process_socialize_services_metrics();
 
         $wrapped_content = "";
-        $bitly_content = "";
-        $og_content = "";
+        
+        $sharrre_content = "";
+        $table_container = "";
+        $jquery_content = "";
+        $jquery_container = "";
+        
+        $the_query = new WP_Query(
+                array(
+                    'post_type' => 'any',
+                    'posts_per_page' => '-1',
+                    'post_status' => 'publish'
+                    )
+                );
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+            $id = 'sharrre' . get_the_ID();
+            $sharrre_content .= '<div id="'.$id.'"></div>';
+            $jquery_content .= "shareChart('".$id."', '". get_permalink() ."', '". get_the_title() ."');" . "\n";
+            
+        endwhile;
+        wp_reset_postdata();
+        $table_container .= '<table id="metrics-table"><tr>';
+        $table_container .= '<td>Title</td>';
+        $table_container .= '<td>Total</td>';
+        $table_container .= '<td>Google+</td>';
+        $table_container .= '<td>Facebook</td>';
+        $table_container .= '<td>Twitter</td>';
+        $table_container .= '<td>Digg</td>';
+        $table_container .= '<td>Delicious</td>';
+        $table_container .= '<td>Stumbleupon</td>';
+        $table_container .= '<td>Linkedin</td>';
+        $table_container .= '<td>Pinterest</td>';
+        $table_container .= '</tr></table>';
+        $table_container .= $sharrre_content;
 
-        if (function_exists('wp_nonce_field')) {
-            $bitly_content .= wp_nonce_field('socialize-update-social-metrics', '_wpnonce', true, false);
-        }
+        $jquery_container .= '<script>jQuery(document).ready(function($) {' . "\n";
+        $jquery_container .= $jquery_content . "\n";
+        $jquery_container .= '});</script>';
 
-        $bitly_content .= '<p>' . __("Bitly Username") . '<br />
-					<input type="text" name="socialize_bitly_name" value="' . $socialize_settings['socialize_bitly_name'] . '" /></p>';
-        $bitly_content .= '<p>' . __("Bitly API Key") . '<br />
-					<input type="text" name="socialize_bitly_key" value="' . $socialize_settings['socialize_bitly_key'] . '" />
-					<small>If you have a Bitly account, you can find your API key <a href="http://bit.ly/a/your_api_key/" target="_blank">here</a>.</small></p>';
-        $wrapped_content .= SocializeAdmin::socialize_postbox('socialize-settings-bitly', 'Bitly Settings', $bitly_content);
+        $wrapped_content .= SocializeAdmin::socialize_postbox('socialize-settings-facebook', 'All Shares', $table_container . "\n" . $jquery_container);
 
-        $og_content .= '<p>' . __("Enable Open Graph") . '<br />
-					<input type="checkbox" name="socialize_og" ' . checked($socialize_settings['socialize_og'], 'on', false) . ' />
-					<small>Uncheck this if you do not want to insert <a href="http://developers.facebook.com/docs/opengraph/" target="_blank">open graph</a> meta data into your HTML head.</small></p>';
-        $og_content .= '<p>' . __("Facebook App ID") . '<br />
-				      <input type="text" name="socialize_fb_appid" value="' . $socialize_settings['socialize_fb_appid'] . '" />
-                                      <small>You can set up and get your Facebook App ID <a href="http://www.facebook.com/developers/apps.php" target="_blank">here</a>.</small></p>';
-        $og_content .= '<p>' . __("Facebook Admin IDs") . '<br />
-				      <input type="text" name="socialize_fb_adminid" value="' . $socialize_settings['socialize_fb_adminid'] . '" />
-                                      <small>A comma-separated list of Facebook user IDs. Find it <a href="http://apps.facebook.com/whatismyid" targe="_blank">here</a>.</small></p>';
-
-
-
-        $og_content .= '<p>' . __("Facebook Page ID") . '<br />
-				      <input type="text" name="socialize_fb_pageid" value="' . $socialize_settings['socialize_fb_pageid'] . '" />
-                                      <small>A Facebook Page ID.</small></p>';
-
-
-
-
-        $wrapped_content .= SocializeAdmin::socialize_postbox('socialize-settings-facebook', 'Open Graph Settings', $og_content);
-
-
-        SocializeAdmin::socialize_admin_wrap('Socialize: General Settings', $wrapped_content);
+        SocializeAdmin::socialize_admin_wrap('Socialize: Metrics', $wrapped_content);
     }
 
     //=============================================
